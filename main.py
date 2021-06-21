76aa93f57135960205e7b83c45a8341e7cc8aa90
elsaali <86256450+elsaali@users.noreply.github.com>
2021-06-21 14:30:45 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+print("*** Welcome to Pomodoro Timer ***")
+task = str(input("What task will you be you working on? "))
+studyTime = int(input("How long would you like to study for? "))
+print("Timer for " ,task, " is " ,studyTime, " minutes")
